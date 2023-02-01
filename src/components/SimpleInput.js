39dc62bf53d829d86c77e7a026b1f359a25c4132
unknown <mihaela@matrixinformatika.hr>
2023-02-01 12:00:06 +0100

    Overall form validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -6,7 +6,13 @@ const SimpleInput = (props) => {
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
+  const enterAgeIsValid = true;
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enterAgeIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -50,7 +56,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
